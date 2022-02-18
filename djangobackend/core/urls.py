48fc8dbaa92a9b7ce980493d5366149827d7099c
pugach <pugach@scand.com>
2022-02-18 19:09:12 +0300

    simple actions

--- djangobackend/core/urls.py
+++ djangobackend/core/urls.py
@@ -1,16 +1,19 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
-
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import BookViewSet
 
 router = DefaultRouter()
 router.register(r'books', BookViewSet)
 
 urlpatterns = [
-    path('auth/', include('rest_framework.urls')),
+    # path('create/test/', CreateCategory.as_view()),
     path('api-token/', TokenObtainPairView.as_view()),
     path('api-token-refresh/', TokenRefreshView.as_view()),
     path("", include(router.urls)),
 
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- djangobackend/core/views.py
+++ djangobackend/core/views.py
@@ -1,6 +1,9 @@
-from rest_framework import viewsets
+from django.core.files.uploadedfile import UploadedFile
+from rest_framework import viewsets, status
 from rest_framework.filters import SearchFilter
 # from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Book
 from .serializers import BookSerializer
@@ -14,9 +17,32 @@ class BookViewSet(viewsets.ModelViewSet):
     # filterset_fields = ['prise']
     search_fields = ['name', 'author']
 
-    def perform_create(self, serializer):
+    def perform_create(self, UploadedFile):
         # parserPdf()
-        serializer.save()
+        print("name")
 
-    def post(self, ):
-        pass
+        test = UploadedFile.get('pdf')
+        # test = request.data.get('pdf')
+        print(test)
+
+        return Response('test', status=status.HTTP_201_CREATED)
+
+
+# class CreateCategory(APIView):
+#
+#     def post(self, request, format=None):
+#         # parserPdf()
+#         print("test")
+#
+#         test = request.data.get("test")
+#         print(test)
+#
+#         return Response('test', status=status.HTTP_201_CREATED)
+
+# class CreateCategory(APIView):
+#
+#     def perform_create(self, serializer):
+#         # parserPdf()
+#         print('test', )
+#         serializer.save()
+#         return Response('test', status=status.HTTP_201_CREATED)
